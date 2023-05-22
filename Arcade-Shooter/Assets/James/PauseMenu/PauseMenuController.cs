2fa036e465df3aafaa42eab933bed399c2e22728
ArrowSpawn <jamesapplejuice@gmail.com>
2023-05-23 10:30:26 +1200

    Pause Menu V4 - Esc-to-Pause Works!
    - Pause Buttons Work
    - Press Esc to Pause / Resume
    - Exit button not functional yet
    
    To attach the Pause Menu in a different scene,
    add it to a canvas element.
    See "PauseMenuV3_MenuWorks.unity" for an example

--- Arcade-Shooter/Assets/James/PauseMenu/PauseMenuController.cs
+++ Arcade-Shooter/Assets/James/PauseMenu/PauseMenuController.cs
@@ -2,30 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PauseMenuController : MonoBehaviour
-{
-    /**
-    We have a boolean, isPaused. This means we dont have to do
-    any weird stuff checking the game speed, like in the previous version. 
-    */
-    private int isPaused;
-    [SerializeField] private GameObject pauseUI; // The Pause Menu is hidden by default, but we're gonna enable/disable it when we pause/resume
+/**
+Overview of PauseMenuController.cs:
+    _______________________________________________
+    1. --> private int isPaused;
 
-    /** the Pause() function has 1 job, to pause the game:
+    This variable is very important, it means we dont have to do
+    any weird stuff like checking the game speed
+    because that would be very bad code practice lol
+    _______________________________________________
+    2. pauseUI --> [SerializeField] private GameObject pauseUI;
+    
+    The Pause Menu is hidden by default,
+    but we're gonna enable/disable it when we pause/resume
+    _______________________________________________
+    3. Pause() --> public void Pause()
+    
+    The Pause() function has 1 job, to pause the game:
     1. When game is resumed, the Pause() function pauses the game and show a pause menu
     2. When the game is resumed, the Pause() function will instead hide the pause menu
     and resume gameplay.
     3. (Maybe fade in the pause menu? Could be nice?)
 
     
+    Made some Pseudocode for reference:
 
     if the game isPaused (i.e. the timeScale, speed of the game, is stopped (0:1 speed)):
         Resume the game by setting speed to be 1:1
 
     Else
         Pause the game by setting speed to be 0:1
-    */
-    public void Pause()
+    _______________________________________________
+    4. Exit() --> public void Exit()
+    DONT NEED THIS RIGHT NOW CAUSE SCENE-CHANGING WILL COME LATER
+
+    _______________________________________________
+    5. update() --> void update()
+    Keypress Pausing, it's handled here.
+    Any time the user presses the Pause key (ie. ESC)
+    the game will pause
+*/
+public class PauseMenuController : MonoBehaviour
+{
+    //Variables:
+    private int isPaused;
+    [SerializeField] private GameObject pauseUI; // The Pause Menu is hidden by default, but we're gonna enable/disable it when we pause/resume
+
+    //Methods PauseResume() and update():
+    public void PauseResume()
     {
         float scale = Time.timeScale;
         //1. When resumed,
@@ -48,8 +72,16 @@ public class PauseMenuController : MonoBehaviour
         }
     }
 
-    public void Exit()
+    /**
+    if Esc key pressed, the game will pause
+    */
+    void Update()
     {
-
+        //Debug.Log("UPDATUNBG!!");
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Esc Key Pressed! Game should pause/resume");
+            this.PauseResume();
+        }
     }
 }
